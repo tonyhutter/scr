4e89fd91345651e7303567a2e44a6a30d99dcb7c
Adam Moody <moody20@llnl.gov>
2016-12-21 13:33:29 -0800

    support multiple sets of brackets in nodelist

--- scripts/common/scr_hostlist.pm
+++ scripts/common/scr_hostlist.pm
@@ -19,50 +19,128 @@ sub expand {
   }
   my $nodeset = shift @_;
 
-  my $machine = undef;
-  my @lowhighs = ();
-  my $numberLength = 0; # for leading zeros, e.g atlas[0001-0003]
-  if ($nodeset =~ /([\D]*)\[([\d,-]+)\]/) {
-    # hostlist with brackets, e.g., atlas[2-5,28,30]
-    $machine = $1;
-    my @ranges = split ",", $2;
-    foreach my $range (@ranges) {
-      my $low  = undef;
-      my $high = undef;
-      if ($range =~ /(\d+)-(\d+)/) {
-        # low-to-high range
-        $low  = $1;
-        $high = $2;
+  my @nodes = ();
+
+  # split entries on commas
+  # machine[1-3,5],machine[7-8],machine10
+  my @chunks = split ",", $nodeset;
+  while (@chunks > 0) {
+    # look for opening bracket
+    my $chunk = shift @chunks;
+    if ($chunk =~ /^(.*)\[(.*)$/) {
+      # got a starting bracket, scan until we find the closing bracket
+      my $prefix  = $1;
+      my $content = $2;
+      my $suffix  = "";
+
+      # build a list of ranges until we find the closing bracket
+      my @ranges = ();
+      if ($content =~ /^(.*)\](.*)$/) {
+        # found the closing bracket (and optional suffix) in the same chunk
+        push @ranges, $1;
+        $suffix = $2;
       } else {
-        # single element range
-        $low  = $range;
-        $high = $range;
+        # no bracket, so we either got a single number or a range here
+        push @ranges, $content;
+
+        # pluck off entries until we find the closing bracket
+        while ($chunks[0] !~ /^(.*)\](.*)$/) {
+          $chunk = shift @chunks;
+          push @ranges, $chunk;
+        }
+
+        # if well formed, this item must now have the bracket
+        $chunk = shift @chunks;
+        if ($chunk =~ /^(.*)\](.*)$/) {
+          # found the closing bracket (and optional suffix)
+          push @ranges, $1;
+          $suffix = $2;
+        }
       }
-      #if the lowest number starts with 0
-      if($numberLength == 0 and index($low,"0") == 0){ 
-         $numberLength = length($low);
+
+      # expand ranges to pairs of low/high values
+      my @lowhighs = ();
+      my $numberLength = 0; # for leading zeros, e.g atlas[0001-0003]
+      foreach my $range (@ranges) {
+        my $low  = undef;
+        my $high = undef;
+        if ($range =~ /(\d+)-(\d+)/) {
+          # low-to-high range
+          $low  = $1;
+          $high = $2;
+        } else {
+          # single element range
+          $low  = $range;
+          $high = $range;
+        }
+        #if the lowest number starts with 0
+        if($numberLength == 0 and index($low,"0") == 0){ 
+           $numberLength = length($low);
+        }
+        push @lowhighs, $low, $high;
       }
-      push @lowhighs, $low, $high;
+
+      # produce our list of node names
+      while(@lowhighs) {
+        my $low  = shift @lowhighs;
+        my $high = shift @lowhighs;
+        for(my $i = $low; $i <= $high; $i++) {
+          # tack on leading 0's if input had them
+          my $nodenumber = sprintf("%0*d", $numberLength, $i);
+          my $nodename = $prefix . $nodenumber . $suffix;
+          push @nodes, $nodename;
+        }
+      }
+    } else {
+      # no brackets, just a single node name, copy it verbatim
+      push @nodes, $chunk;
     }
-  } else {
-    # single node hostlist, e.g., atlas2
-    $nodeset =~ /([\D]*)(\d+)/;
-    $machine = $1;
-    $numberLength = length($2);
-    push @lowhighs, $2, $2;
   }
 
-  # produce our list of nodes
-  my @nodes = ();
-  while(@lowhighs) {
-    my $low  = shift @lowhighs;
-    my $high = shift @lowhighs;
-    for(my $i = $low; $i <= $high; $i++) {
-      my $nodenumber = sprintf("%0*d", $numberLength, $i);
-      #print $nodenumber;
-      push @nodes, $machine . $nodenumber;
-    }
-  }
+#  my $machine = undef;
+#  my @lowhighs = ();
+#  my $numberLength = 0; # for leading zeros, e.g atlas[0001-0003]
+#  if ($nodeset =~ /([\D]*)\[([\d,-]+)\]/) {
+#    # hostlist with brackets, e.g., atlas[2-5,28,30]
+#    $machine = $1;
+#    my @ranges = split ",", $2;
+#    foreach my $range (@ranges) {
+#      my $low  = undef;
+#      my $high = undef;
+#      if ($range =~ /(\d+)-(\d+)/) {
+#        # low-to-high range
+#        $low  = $1;
+#        $high = $2;
+#      } else {
+#        # single element range
+#        $low  = $range;
+#        $high = $range;
+#      }
+#      #if the lowest number starts with 0
+#      if($numberLength == 0 and index($low,"0") == 0){ 
+#         $numberLength = length($low);
+#      }
+#      push @lowhighs, $low, $high;
+#    }
+#  } else {
+#    # single node hostlist, e.g., atlas2
+#    $nodeset =~ /([\D]*)(\d+)/;
+#    $machine = $1;
+#    $numberLength = length($2);
+#    push @lowhighs, $2, $2;
+#  }
+#
+#  # produce our list of nodes
+#  my @nodes = ();
+#  while(@lowhighs) {
+#    my $low  = shift @lowhighs;
+#    my $high = shift @lowhighs;
+#    for(my $i = $low; $i <= $high; $i++) {
+#      my $nodenumber = sprintf("%0*d", $numberLength, $i);
+#      #print $nodenumber;
+#      push @nodes, $machine . $nodenumber;
+#    }
+#  }
 
   return @nodes;
 }
@@ -74,53 +152,9 @@ sub compress {
     return "";
   }
 
-  # pull the machine name from the first node name
-  my @numbers = ();
-  my ($machine) = ($_[0] =~ /([\D]*)(\d+)/);
-  foreach my $host (@_) {
-    # get the machine name and node number for this node
-    my ($name, $number) = ($host =~ /([\D]*)(\d+)/);
-
-    # check that all nodes belong to the same machine
-    if ($name ne $machine) {
-      return undef;
-    }
-
-    # record the number
-    push @numbers, $number;
-  }
-
-  # order the nodes by number
-  my @sorted = sort {$a <=> $b} @numbers;
-
-  # TODO: toss out duplicates?
-
-  # build the ranges
-  my @ranges = ();
-  my $low  = $sorted[0];
-  my $last = $low;
-  for(my $i=1; $i < @sorted; $i++) {
-    my $high = $sorted[$i];
-    if($high == $last + 1) {
-      $last = $high;
-      next;
-    }
-    if($last > $low) {
-      push @ranges, $low . "-" . $last;
-    } else {
-      push @ranges, $low;
-    }
-    $low  = $high;
-    $last = $low;
-  }
-  if($last > $low) {
-    push @ranges, $low . "-" . $last;
-  } else {
-    push @ranges, $low;
-  }
-
-  # join the ranges with commas and return the compressed hostlist
-  return $machine . "[" . join(",", @ranges) . "]";
+  # sort the node names and join them with commas
+  my @nodes = sort {$a cmp $b} @_;
+  return join(",", @nodes);
 }
 
 # Given references to two lists, subtract elements in list 2 from list 1 and return remainder
